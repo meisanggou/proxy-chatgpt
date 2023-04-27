954937276cca0e3e1ff5d0c6e8f31365cd852c3f
zhouhenglc <zhouhenglc@inspur.com>
2023-04-27 19:36:08 +0800

    add user view

--- proxy_chatgpt/web/__init__.py
+++ proxy_chatgpt/web/__init__.py
@@ -1,7 +1,10 @@
 # !/usr/bin/env python
 # coding: utf-8
+from flask import session
+
 from flask_helper import Flask2
 from flask_login import LoginManager
+from flask_login import UserMixin
 
 from proxy_chatgpt.utils.log import getLogger
 from proxy_chatgpt.web import env_filters
@@ -14,8 +17,26 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 
 
+class User(UserMixin):
+    user_name = ""
+
+    def get_id(self):
+        return self.user_name
+
+
+@login_manager.user_loader
+def load_user(user_name):
+    user = User()
+    user.user_name = user_name
+    # if "role" not in session:
+    #     session["role"] = 0
+    # user.role = session["role"]
+    return user
+
+
 def get_app():
     app = Flask2(__name__, log=LOG)
+    app.secret_key = 'Proxy-ChatGPT0427'
     login_manager.init_app(app)
     app.real_ip()
     env = app.jinja_env
--- proxy_chatgpt/web/views/user_view.py
+++ proxy_chatgpt/web/views/user_view.py
@@ -1,12 +1,41 @@
 # !/usr/bin/env python
 # coding: utf-8
+from flask import request
+from flask import session
+
 from flask_helper.view import View
+from flask_login import login_user
+
+from proxy_chatgpt.objects.user import UserObject
+from proxy_chatgpt.web import User
 
 __author__ = 'zhouhenglc'
 
 
 user_view = View('user', __name__, url_prefix='/user')
+user_m = UserObject()
+
 
 @user_view.route('/login', methods=['POST'])
 def user_login():
-    return {'status': True}
+    request_data = request.json
+    user_name = request_data["user_name"]
+    password = request_data["password"]
+    r_code, info = user_m.user_confirm(password, user=user_name)
+    if r_code == -3:
+        return {"status": False, "data": "内部错误"}
+    elif r_code == -2:
+        return {"status": False, "data": "账号不存在"}
+    elif r_code == -1:
+        return {"status": False, "data": "密码不正确"}
+    session["role"] = info["role"]
+
+    user = User()
+    user.user_name = info["user_name"]
+    login_user(user, remember=False)
+    if "next" in request_data and request_data["next"] != "":
+        return {"status": True, "data": {"location": request_data["next"], "user_name": user.user_name}}
+    if session["role"] == 0:
+        return {"status": False, "data": "您还没有任何权限，请联系管理员授权"}
+    else:
+        return {"status": True, "data": {"location": "/", "user_name": user.user_name}}
