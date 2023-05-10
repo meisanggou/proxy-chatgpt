f534b59bcee881017ba8cd63c21501194f8797c5
zhouhenglc <zhouhenglc@inspur.com>
2023-05-10 13:14:22 +0800

    update new_user

--- proxy_chatgpt/cmd/new_user.py
+++ proxy_chatgpt/cmd/new_user.py
@@ -5,12 +5,21 @@ from proxy_chatgpt.objects.user import UserObject
 
 __author__ = 'zhouhenglc'
 
+
 def main(*args):
     user = UserObject()
-    if len(args >= 3):
+    if len(args > 3):
         user_name = args[0]
         password = args[1]
         role = args[2]
+    elif len(args > 2):
+        user_name = args[0]
+        password = args[1]
+        role = 1
+    else:
+        print('no do anything')
+        return
+    user.new_user(user_name, password, role=role)
 
 
 if __name__ == '__main__':
